87478c397db626f54100c2bb59f2196f93a65323
Danielle <Danielle@DR-LAP>
2025-09-14 15:40:57 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project/IDE and VCS Workflow Project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char first, middle, last;
+
+    first = 'A';
+    middle = 'R';
+    last = 'C';
+
+    cout << first << " " << middle << " " << last;
+    return 0;
+}
